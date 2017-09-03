23b59334e251f39b83bef9cec7f9d4541b4e163f
Lxopato <lpbustoscarrasco@gmail.com>
2017-09-02 23:12:43 -0300

    Add CSV Parser, add data set and a test set

---
+++ GlassPredictor/CSVparser.py
@@ -0,0 +1,25 @@
+import csv
+
+class CSVparser:
+
+    def __init__(self,file):
+        self.file = file
+        self.reader = list()
+        self.get_file()
+
+    def get_file(self):
+        with open(self.file,'r') as f:
+            self.reader = [row for row in csv.reader(f, delimiter=",")]
+            return self.reader
+
+    def get_data(self):
+        data=[]
+        for row in self.reader:
+            data.append(row[:-1])
+        return data
+
+    def get_results(self):
+        results=[]
+        for row in self.reader:
+            results.append([row[-1]])
+        return results
