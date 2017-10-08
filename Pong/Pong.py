bfed5a1325c35aebd14e8f378cb1ed1d7da87c26
Lxopato <lpbustoscarrasco@gmail.com>
2017-10-08 00:48:11 -0300

    Finished Pong Interface

--- Pong/Pong.py
+++ Pong/Pong.py
@@ -36,19 +36,9 @@ def drawBall(BallXPos,BallYPos):
     pygame.draw.rect(screen,WHITE,ball)
 
 
-def drawPaddle(PaddleYPos):
-    paddle = pygame.rect(PADDLE_BUFFER,PaddleYPos,PADDLE_WIDTH,PADDLE_HEIGHT)
-    pygame.draw.rect(screen,WHITE,paddle)
-
-
-def drawOpponent(OpponentYPos):
-    paddle = pygame.rect(WINDOW_WIDTH-PADDLE_BUFFER-PADDLE_WIDTH,OpponentYPos,PADDLE_WIDTH,PADDLE_HEIGHT)
-    pygame.draw.rect(screen,WHITE,paddle)
-
-
 def updateBallPos(PaddleYPos,OpponentYPos,BallXPos,BallYPos,BallXDir,BallYDir):
-    BallXPos = BallXPos + BallXDir*BALL_X_SPEED
-    BallYPos = BallYPos + BallYDir * BALL_Y_SPEED
+    BallXPos += BallXDir*BALL_X_SPEED
+    BallYPos += BallYDir * BALL_Y_SPEED
     score = 0
 
     if (BallXPos <= PADDLE_BUFFER + PADDLE_WIDTH and PaddleYPos <= BallYPos + BALL_HEIGHT and  PaddleYPos + PADDLE_HEIGHT >= BallYPos - BALL_HEIGHT ):
@@ -73,3 +63,109 @@ def updateBallPos(PaddleYPos,OpponentYPos,BallXPos,BallYPos,BallXDir,BallYDir):
         BallYDir = -1
 
     return [score, PaddleYPos, OpponentYPos, BallXPos, BallYPos, BallXDir, BallYDir]
+
+
+def drawPaddle(PaddleYPos):
+    paddle = pygame.rect(PADDLE_BUFFER, PaddleYPos, PADDLE_WIDTH, PADDLE_HEIGHT)
+    pygame.draw.rect(screen, WHITE, paddle)
+
+
+def updatePaddle(action, PaddleYPos):
+    if (action[1] == 1):
+        PaddleYPos -= PADDLE_SPEED
+
+    if (action[2] ==1):
+        PaddleYPos += PADDLE_SPEED
+
+    if (PaddleYPos <0):
+        PaddleYPos = 0
+
+    if (PaddleYPos > WINDOW_HEIGHT - PADDLE_HEIGHT):
+        PaddleYPos = WINDOW_HEIGHT - PADDLE_HEIGHT
+
+    return PaddleYPos
+
+
+def drawOpponent(OpponentYPos):
+    paddle = pygame.rect(WINDOW_WIDTH-PADDLE_BUFFER-PADDLE_WIDTH,OpponentYPos,PADDLE_WIDTH,PADDLE_HEIGHT)
+    pygame.draw.rect(screen,WHITE,paddle)
+
+
+def updateOpponent(OpponentYPos, BallYPos):
+
+    if (OpponentYPos +PADDLE_HEIGHT/2 < BallYPos +BALL_HEIGHT/2):
+        OpponentYPos += PADDLE_SPEED
+
+    if (OpponentYPos +PADDLE_HEIGHT/2 > BallYPos +BALL_HEIGHT/2):
+        OpponentYPos -= PADDLE_SPEED
+
+    if (OpponentYPos <0):
+        OpponentYPos = 0
+
+    if (OpponentYPos > WINDOW_HEIGHT - PADDLE_HEIGHT):
+        OpponentYPos = WINDOW_HEIGHT - PADDLE_HEIGHT
+
+    return OpponentYPos
+
+class Pong:
+    def __init__(self):
+        self.tally =0
+        self.paddleYPos = WINDOW_HEIGHT/2 - PADDLE_HEIGHT/2
+        self.opponentYPos = WINDOW_HEIGHT/2 - PADDLE_HEIGHT/2
+        self.ballXPos = WINDOW_WIDTH/2 - BALL_WIDTH/2
+        self.ballYPos = random.randint(0,4)*(WINDOW_HEIGHT-BALL_HEIGHT)/4
+        self.ballXDir = 1
+        self.ballYDir = 1
+        self.randomDir()
+
+    def randomDir(self):
+        num = random.randint(0,3)
+        if (num == 0):
+            self.ballXDir = 1
+            self.ballYDir = 1
+        elif (num == 1):
+            self.ballXDir = -1
+            self.ballYDir = 1
+        elif (num == 2):
+            self.ballXDir = 1
+            self.ballYDir = -1
+        elif (num == 3):
+            self.ballXDir = -1
+            self.ballYDir = -1
+
+    def getPresentFrame(self):
+        pygame.event.pump()
+        screen.fill(BLACK)
+
+        drawPaddle(self.paddleYPos)
+        drawOpponent(self.opponentYPos)
+        drawBall(self.ballXPos,self.ballYPos)
+
+        data = pygame.surfarray.array3d(pygame.display.get_surface())
+
+        pygame.display.flip()
+
+        return data
+
+    def getNextFrame (self, action):
+        pygame.event.pump()
+        score = 0
+        screen.fill(BLACK)
+
+        self.paddleYPos = updatePaddle(action, self.paddleYPos)
+        drawPaddle(self.paddleYPos)
+
+        self.opponentYPos = updateOpponent(self.opponentYPos, self.ballYPos)
+        drawOpponent(self.opponentYPos)
+
+        [score , self.paddleYPos, self.opponentYPos, self.ballXPos, self.ballYPos, self.ballXDir, self.ballYDir] = updateBallPos(self.paddleYPos, self.opponentYPos , self.ballXPos , self.ballYPos, self.ballXDir, self.ballYDir)
+
+        drawBall(self.ballXPos, self.ballYPos)
+
+        data = pygame.surfarray.array3d(pygame.display.get_surface())
+
+        pygame.display.flip()
+
+        self.tally += score
+        print("Tally is " + str(self.tally))
+        return [score, data]
