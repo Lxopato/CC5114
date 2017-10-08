383d55b6811f5ea1f44ddf1d384b891f0b7f07b2
Lxopato <lpbustoscarrasco@gmail.com>
2017-10-08 01:31:11 -0300

    Add file for Neural Network

---
+++ Pong/NeuralPong.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+import cv2
+import Pong
+import numpy as np
+import random
+from collections import deque
+
+def createGraph():
+    return
+
+def trainGraph(inp, out, session):
+    return
+
+def main():
+    session = tf.InteractiveSession()
+    inp,out = createGraph()
+    trainGraph(inp,out,session)
+
+if __name__ == "__main__":
+    main()
